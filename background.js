5b53f08bef2aa8c88bcbc474dac2eba5d85698ff
ByeongWooCho <whquddn55@gmail.com>
2021-10-02 21:20:59 +0900

    Version 2.0

--- background.js
+++ background.js
@@ -1,52 +1,210 @@
+let requestList = []
+let progressMap = {}
+let fileObj = {}
+
+chrome.storage.local.get(['progressMap'], (result) => {
+	progressMap = result.progressMap || {}
+})
+
+chrome.webRequest.onBeforeRequest.addListener(
+	async (details) => {
+		if (details['initiator'] && details['initiator'].indexOf('chrome-extension://') != -1)
+			return
+		if (requestList.length >= 1000)
+			requestList.splice(0, 500)
+		if (details['url'] && (details['url'].indexOf('m3u8') != -1)) {
+			let token = details['url'].substr(0, details['url'].indexOf('m3u8'))
+			for (let e of requestList)
+				if (e['token'] == token)
+					return
+			requestList.push({url : details['url'], source: details['initiator'], token: token})
+		}
+	},
+	{urls: ["<all_urls>"]}, []
+);
 
 chrome.runtime.onMessage.addListener(
 	async (request, sender, sendResponse) => {
 		if (request.action === 'DOWNLOAD') {
-			const url = await getm3u8(request.url)
-			console.log(url, request.title)
-			downloadVideo(url, request.title, request.className, sender.tab.id)
+			downloadVideo(request.url, request.title)
+		}
+		else if (request.action === 'FILELIST') {
+			sendResponse({fileList: fileObj[request.tabId]})
+		}
+		else if (request.action === 'PROGRESS') {
+			sendResponse({progressMap})
+		} 
+		else if (request.action === 'DELETE') {
+			delete progressMap[request.target]
+			chrome.storage.local.set({progressMap})
+			sendResponse({result: true})
 		}
 	});
 
-function downloadNormalFile(info) {
-	if (info.pageUrl.indexOf('https://plato.pusan.ac.kr/') == -1)
-		return
-	let xhr = new XMLHttpRequest();
-	xhr.open('GET', info.linkUrl, true);
-	xhr.onreadystatechange = function () { 
-		if (this.readyState == 2) {
-			let filename = this.responseURL.split('?')
-			filename.pop()
-			filename = filename.join('?').split('/')
-			filename = decodeURI(filename.pop())
+// tab url 변경 시
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+	// 초기화
+	if (changeInfo.status == 'loading') {
+		chrome.browserAction.setBadgeText({text: '0'})
+		delete fileObj[tabId]
+	}
+	if (changeInfo.status == 'complete' && tab.url) {
+		let fileList = await getFileList(tab.url, tab)
+		fileObj[tabId] = fileList
+		chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+			if (tabs && tabs[0].id == tabId)
+				chrome.browserAction.setBadgeText({text: String(fileObj[tabId].length)})
+		})
+	}
+})
 
-			let xhr2 = new XMLHttpRequest();
-			xhr2.open('GET', this.responseURL, true)
-			xhr2.responseType = "blob"
-			xhr2.onreadystatechange = function() {
-				if (xhr2.readyState === xhr2.DONE) {
-					if (xhr2.status === 200 || xhr2.status === 201) {
-						let a = document.createElement('a')
-						a.download = filename
-						a.href = URL.createObjectURL(xhr2.response)
-						a.style.display = 'none'
-						document.body.appendChild(a)
-						a.click()
-						a.remove()
+// tab change 시
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+	// icon badge추가
+	chrome.browserAction.setBadgeText({text: String(fileObj[activeInfo.tabId] ? fileObj[activeInfo.tabId].length : 0)})
+
+});
+ 
+function downloadTS(tsUrlList, finishList, fileName, tsUrl) {
+	let downloadIndex = 0
+	let mediaFileList = []
+	let finishNum = 0
+
+	let calculateColor = (f, e) => {
+		const hue = (f / e * 120).toString(10);
+		return ["hsl(", hue, ",80%,50%)"].join("");
+	}
+
+	let calculateValue = (f, e) => {
+		if (f == e) 
+			return '완료!'
+		let value = parseInt((f / e) * 100)
+		let res = value
+		let cnt = 3
+		while (value) {
+			value = parseInt(value / 10)
+			--cnt
+		}
+		while (cnt--) {
+			res += ' '
+		}
+		return res + '%'
+	}
+
+	let downloadFile = (fileDataList) => {
+		let fileBlob = null
+		let a = document.createElement('a')
+		fileBlob = new Blob(fileDataList, { type: 'video/mp4' })
+		a.download = fileName + '.mp4'
+		a.href = URL.createObjectURL(fileBlob)
+		a.style.display = 'none'
+		document.body.appendChild(a)
+		a.click()
+		a.remove()
+	}
+	
+	let dealTS = (file, index, callback) => {
+		mediaFileList[index] = file
+		finishList[index].status = 'finish'
+		finishNum++
+		const color = calculateColor(finishNum, tsUrlList.length)
+		const value = calculateValue(finishNum, tsUrlList.length)
+		progressMap[fileName] = {
+			progress: value, color
+		}
+
+		if (value == '완료!') {
+			chrome.storage.local.set({progressMap})
+		}
+
+		if (finishNum === tsUrlList.length) {
+			downloadFile(mediaFileList)
+		}
+		callback()
+	}
+
+	let forceStop = false
+	let download = (index, depth) => {
+		if (forceStop)
+			return
+		if (index == undefined){
+			index = downloadIndex
+			downloadIndex++
+		} 
+		depth = depth || 1
+		if (finishList[index] && finishList[index].status === '') {
+			ajax({
+				url: tsUrlList[index],
+				type: 'file',
+				success: (file) => {
+					if (forceStop)
+						return
+					dealTS(file, index, () => index < tsUrlList.length && download())
+				},
+				fail: () => {
+					if (forceStop)
+						return
+					if (depth == 10) {
+						window.prompt('다운로드 실패! 아래 정보와 함께 개발자에게 연락 바랍니다.', `${index} : ${downloadIndex} : ${finishList[index].title} : ${tsUrl}`)
+						forceStop = true
+					} else {
+						download(index, depth + 1)
 					}
 				}
-			};
-			xhr2.send()
+			})
+		} 
+		else if (downloadIndex < tsUrlList.length) {
+			console.log('Unexpected Error')
+			console.log(downloadIndex, tsUrlList.length)
+			console.log(index, finishList[index].status)
 		}
-	};
+	}
 
-	xhr.send()
+	for (let i = 0; i < 10; i++) {
+		download()
+	}
 }
 
-chrome.contextMenus.create(
-	{
-		title: "PLATO_자료강제다운", 
-		contexts:["link"], 
-		onclick: downloadNormalFile
+async function downloadVideo(url, fileName) {
+
+	function applyURL(targetURL, baseURL) {
+		baseURL = baseURL || location.href
+		if (targetURL.indexOf('http') === 0) {
+			  return targetURL
+		} else if (targetURL[0] === '/') {
+			  let domain = baseURL.split('/')
+			  return domain[0] + '//' + domain[2] + targetURL
+		} else {
+			  let domain = baseURL.split('/')
+			  domain.pop()
+			  return domain.join('/') + '/' + targetURL
+		}
 	}
-);
+
+	ajax({
+		url: url,
+		type:'get',
+		success: (m3u8Str) => {
+			let tsUrlList = []
+			let finishList = []
+			m3u8Str.split('\n').forEach((item) => {
+				if (item.toLowerCase().indexOf('.ts') > -1) {
+					tsUrlList.push(applyURL(item, url))
+					finishList.push({
+						title: item,
+						status: ''
+					})
+				}
+			})
+			if (tsUrlList.length > 0) { 
+				progressMap[fileName] = {
+					progress : '0  %', color: '#FF0000'
+				}
+				chrome.runtime.sendMessage({action: 'PROGRESS', title: fileName, progress : '0  %', color: "#FF0000"});
+				downloadTS(tsUrlList, finishList, fileName, url)
+			} else {
+				window.alert('에러코드 : 0x80808080')
+			}
+		}
+	})
+}
--- popup.js
+++ popup.js
@@ -1,35 +1,202 @@
+let reverseStatus = 0
+let hideStatus = 0
 
-chrome.runtime.onMessage.addListener(
-	async (request, sender, sendResponse) => {
-		if (request.action === 'PROGRESS') {
-			disableLoader()
-			setProgress(request.title, request.progress, request.color)
+function ajax(options) {
+	options = options || {};
+	let xhr = new XMLHttpRequest();
+	if (options.type === 'file') {
+		xhr.responseType = 'arraybuffer';
+	}
+
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+		let status = xhr.status;
+		if (status >= 200 && status < 300) {
+			options.success && options.success(xhr.response);
+		} else {
+			options.fail && options.fail(status);
 		}
-	});
+		}
+	};
 
-function disableLoader() {
-	document.getElementById('loader').setAttribute('style', 'display:none;')
+	xhr.open("GET", options.url, true);
+	xhr.send(null);
 }
 
-function createNewItemBox(title, progressValue, color) {
+async function downloadButtonClickEvent(downloadButton, url, title, subTitle) {
+	downloadButton.style.pointerEvents = 'none';
+	chrome.runtime.sendMessage({action: 'DOWNLOAD', url: url, title: title + '_' + subTitle});
+}
+
+function appendFileItem(param) {
+	const title = param.title ? param.title : ''
+	const subTitle = param.subTitle ? param.subTitle : ''
+	const url = param.url ? param.url : ''
+	const reserve = param.reserve ? param.reserve : false
+
+	let fileListItem = document.createElement('div')
+	fileListItem.className = 'item'
+
+	let titleItem = document.createElement('div')
+	titleItem.textContent = title
+	titleItem.className = 'itemTitle'
+
+	let subTitleItem = document.createElement('div')
+	subTitleItem.textContent = subTitle
+	subTitleItem.className = 'itemSubTitle'
+
+	let downloadButton = document.createElement('div')
+	downloadButton.setAttribute('type', 'button')
+	downloadButton.innerText = '다운로드'
+	downloadButton.setAttribute('id', title + '_' + subTitle)
+	downloadButton.setAttribute('class', 'downloadButton')
+	downloadButton.onclick = async () => downloadButtonClickEvent(downloadButton, reserve? url : await getm3u8(url), title, subTitle)
+	
+	fileListItem.appendChild(downloadButton)
+	fileListItem.appendChild(titleItem)
+	fileListItem.appendChild(subTitleItem)
+
+	let targetOfList = document.getElementById('fileList')
+	targetOfList.appendChild(fileListItem)
+}
+
+async function requestFileList() {
+	return new Promise((resolve, reject) => {
+		chrome.tabs.query({active:true, currentWindow: true}, (tabs) => {
+			chrome.runtime.sendMessage({action: 'FILELIST', tabId: tabs[0].id}, (res) => {
+				if (res.fileList && res.fileList.length) {
+					for (let e of res.fileList)
+						appendFileItem(e)
+				} else {
+					let emptyMessage = document.createElement('div')
+					emptyMessage.setAttribute('style', 'font-size: 14px; text-align: center; border-top: 1px solid #CCC; margin: 4px 0px; padding: 2px 5px;')
+					emptyMessage.innerText = '동영상을 찾을 수 없습니다.'
+					document.getElementById('fileList').appendChild(emptyMessage)
+				}
+				resolve()
+			})
+		})
+		
+	})
+	
+}
+
+function getm3u8(videoUrl) {
+	return new Promise((resolve, reject) => {
+		ajax({
+			url: videoUrl,
+			type:'get',
+			success: (content) => {
+				const arr = content.split('\n')
+				for (e of arr) {
+					if (e.indexOf('m3u8') != -1) {
+						resolve(e.split('"')[1])
+					}
+				}
+			}
+		})
+	})
+}
+
+function intervalRun() {
+	chrome.runtime.sendMessage({action: 'PROGRESS'}, (res) => {
+		for (let key in res['progressMap']) 
+			setProgress(key, res['progressMap'][key]['progress'], res['progressMap'][key]['color'])
+		
+		for (let e of document.getElementsByClassName('downloadButton')) {
+			if (res['progressMap'][e.id]) {
+				e.innerText = res['progressMap'][e.id]['progress']
+				e.style.color = res['progressMap'][e.id]['color']
+
+				if (res['progressMap'][e.id]['progress'] == '완료!') {
+					e.style.pointerEvents = '';
+					e.parentElement.classList.add('progress-complete')
+					e.parentElement.style.display = hideStatus ? 'none' : ''
+				}
+			} else {
+				e.innerText = '다운로드'
+				e.style.color = '#FFF'
+				e.style.pointerEvents = '';
+				e.parentElement.classList.remove('progress-complete')
+				e.parentElement.style.display = ''
+			}
+		}
+	})
+}
+
+function timer() {
+	intervalRun()
+	setInterval(() => {
+		intervalRun()
+	}, 500);
+}
+
+function reverseList(item){
+	let newItem = item.cloneNode(false)
+	let list = []
+	for (let i = item.children.length - 1; i >= 0; --i) {
+		list.push(item.children[i])
+	}
+	for (let e of list)
+		newItem.appendChild(e)
+	item.parentNode.replaceChild(newItem, item)
+}
+
+function setProgress(title, progress, color) {
+	const target = document.getElementById(title + '_progress')
+	const progressValue = progress.indexOf('%') == -1 ? 100 : parseInt(progress.split('%')[0])
+	if (target == null) {
+		const children = document.getElementById('queue').children
+		let idx = -1
+		for (child of children) 
+			if (child.id <= title) 
+				++idx
+		document.getElementById('queue').insertBefore(createNewProgressBox(title, progressValue, color), children[idx + 1])
+	}
+	else{
+		target.style.setProperty('--progressBar-background', color)
+		target.getElementsByClassName('progressBar')[0].setAttribute('value', progressValue)
+		target.getElementsByClassName('progressItem-right')[0].innerText = progressValue + '%'
+		if (progressValue == 100) {
+			target.classList.add('progress-complete')
+			target.style.display = hideStatus ? 'none' : ''
+		}
+	}
+}
+
+function createNewProgressBox(title, progressValue, color) {
 	let newItemBox = document.createElement('div')
-	newItemBox.setAttribute('class', 'itemBox')
-	newItemBox.setAttribute('id', title)
+	newItemBox.setAttribute('class', 'progressBox')
+	newItemBox.setAttribute('id', title + '_progress')
+	newItemBox.setAttribute('title', '목록에서 삭제하기')
 	newItemBox.style.setProperty('--progressBar-background', color)
 	let newItemLeft = document.createElement('div')
-	newItemLeft.setAttribute('class', 'item-left')
+	newItemLeft.setAttribute('class', 'progressItem-left')
 	let newTitle = document.createElement('div')
-	newTitle.setAttribute('class', 'title')
+	newTitle.setAttribute('class', 'progressItem-title')
 	newTitle.innerText = title
 	let newProgressBar = document.createElement('progress')
 	newProgressBar.setAttribute('class', 'progressBar')
 	newProgressBar.setAttribute('value', progressValue)
 	newProgressBar.setAttribute('max', 100)
 	let newItemRight = document.createElement('div')
-	newItemRight.setAttribute('class', 'item-right')
+	newItemRight.setAttribute('class', 'progressItem-right')
 	newItemRight.innerText = progressValue + '%'
 
+	if (progressValue == 100) {
+		newItemBox.classList.add('progress-complete')
+		newItemBox.style.display = hideStatus ? 'none' : ''
+	}
 
+	newItemBox.onclick = (event) => {
+		event.preventDefault()
+		if (newProgressBar.getAttribute('value') == 100 && confirm(`${title}을\n목록에서 삭제합니다.`))
+			chrome.runtime.sendMessage({action: 'DELETE', target: title}, (res) => {
+				document.getElementById('queue').innerHTML = ''
+				intervalRun()
+			})
+	}
+	
 	newItemBox.appendChild(newItemLeft)
 	newItemBox.appendChild(newItemRight)
 	newItemLeft.appendChild(newTitle)
@@ -37,20 +204,54 @@ function createNewItemBox(title, progressValue, color) {
 	return newItemBox
 }
 
-function setProgress(title, progress, color) {
-	const target = document.getElementById(title)
-	const progressValue = progress.indexOf('%') == -1 ? 100 : parseInt(progress.split('%')[0])
-	if (target === null) {
-		const children = document.getElementById('queue').children
-		let idx = -1
-		for (child of children) 
-			if (child.id <= title) 
-				++idx
-		document.getElementById('queue').insertBefore(createNewItemBox(title, progressValue, color), children[idx + 1])
-	}
-	else{
-		target.style.setProperty('--progressBar-background', color)
-		target.getElementsByClassName('progressBar')[0].setAttribute('value', progressValue)
-		target.getElementsByClassName('item-right')[0].innerText = progressValue + '%'
-	}
+function switchToFileList() {
+	document.getElementsByClassName('tabItem')[0].classList.add('selected')
+	document.getElementsByClassName('tabItem')[1].classList.remove('selected')
+	document.getElementById('queue').style.display = 'none'
+	document.getElementById('reverseItem').style.display = ''
+	document.getElementById('fileList').style.display = ''
 }
+
+function switchToDownloadStatus() {
+	document.getElementsByClassName('tabItem')[1].classList.add('selected')
+	document.getElementsByClassName('tabItem')[0].classList.remove('selected')
+	document.getElementById('fileList').style.display = 'none'
+	document.getElementById('reverseItem').style.display = 'none'
+	document.getElementById('queue').style.display = ''
+}
+
+async function main() {
+	// 우클릭 방지
+	document.oncontextmenu = (event) => event.preventDefault()
+
+	chrome.storage.local.get(['reverseStatus', 'hideStatus'], (result) => {
+		reverseStatus = result.reverseStatus || 0
+		hideStatus = result.hideStatus || 0
+		document.getElementById('hide').setAttribute('src', hideStatus ? 'assets/hided.png' : 'assets/hide.png')
+	})
+
+	await requestFileList()
+	timer()
+
+	if (reverseStatus) 
+		reverseList(document.getElementById('fileList'))
+
+	document.getElementById('reverseItem').onclick = () => {
+		reverseStatus = !reverseStatus
+		chrome.storage.local.set({reverseStatus})
+		reverseList(document.getElementById('fileList'))
+	}
+	document.getElementsByClassName('tabItem')[0].onclick = () => switchToFileList()
+	document.getElementsByClassName('tabItem')[1].onclick = () => switchToDownloadStatus()
+
+	document.getElementById('hide').onclick = () => {
+		hideStatus = !hideStatus
+		chrome.storage.local.set({hideStatus})
+		for (let e of document.getElementsByClassName('progress-complete')) 
+			e.style.display = hideStatus ? 'none' : ''
+		document.getElementById('hide').setAttribute('src', hideStatus ? 'assets/hided.png' : 'assets/hide.png')
+	}
+	switchToFileList()
+}
+
+main()
